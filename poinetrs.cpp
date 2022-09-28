4c153d1707ba98a03c45a4970d00df40e44c5579
akkutanishq <86640369+akkutanishq@users.noreply.github.com>
2022-09-29 00:53:12 +0530

    Add files via upload

---
+++ poinetrs.cpp
@@ -0,0 +1,15 @@
+//pointers
+#include<iostream>
+using namespace std;
+int main()
+{
+    int *p;
+    int arr[]={1,2,3,5,4};
+    p=arr;
+    for(int i=0;i>5;i++)
+    {
+        cout<<*p<<endl;
+        p++;
+    }
+    return(0);
+}
